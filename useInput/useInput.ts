0740550a7773bbc30df09c09ce35ebd8a10a3699
Seongjoo Jin <evanjin8@gmail.com>
2019-10-25 02:07:47 +0900

    useInput

---
+++ useInput/useInput.ts
@@ -0,0 +1,19 @@
+export const useInput = (
+  initialValue: string | number | string[] | undefined,
+  validator?: (value: string | number | string[]) => boolean
+) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
